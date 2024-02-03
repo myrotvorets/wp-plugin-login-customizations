b7547088837057c2b0c67665fd8848cfbeb60bb5
Myrotvorets <support@myrotvorets.center>
2024-02-03 17:38:00 +0200

    Initial import

---
+++ inc/class-plugin.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Myrotvorets\WordPress\LoginCustomization;
+
+final class Plugin {
+	private static ?self $instance = null;
+
+	public static function instance(): self {
+		if ( ! self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+		$this->login_init();
+	}
+
+	public function login_init(): void {
+		add_action( 'login_head', [ $this, 'login_head' ] );
+		add_filter( 'login_headerurl', [ $this, 'login_headerurl' ] );
+		add_filter( 'login_redirect', [ $this, 'login_redirect' ] );
+		add_filter( 'login_title', [ $this, 'login_title' ] );
+		add_filter( 'shake_error_codes', '__return_empty_array' );
+	}
+
+	public function login_head(): void {
+		echo '<style>body.login div#login h1 a{width:180px;height:180px;background-size:180px;background-image:url(https://cdn.myrotvorets.center/m/logos/myrotvorets-180.png)}</style>';
+	}
+
+	/**
+	 * Filters link URL of the header logo above login form.
+	 */
+	public function login_headerurl(): string {
+		return site_url();
+	}
+
+	/**
+	 * Filters the login redirect URL.
+	 */
+	public function login_redirect(): string {
+		return home_url();
+	}
+
+	/**
+	 * Filters the title tag content for login page.
+	 */
+	public function login_title(): string {
+		// translators: %s: Site title
+		return sprintf( __( 'Log In &lsaquo; %s' ), get_bloginfo( 'name', 'display' ) );
+	}
+}
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Myrotvorets\WordPress\LoginCustomization\Plugin;
+
+// @codeCoverageIgnoreStart
+if ( defined( 'ABSPATH' ) ) {
+	if ( defined( 'VENDOR_PATH' ) ) {
+		/** @psalm-suppress UnresolvableInclude, MixedOperand */
+		require_once constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
+	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require_once __DIR__ . '/vendor/autoload.php';
+	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
+		require_once ABSPATH . 'vendor/autoload.php';
+	}
+
+	add_action( 'login_init', [ Plugin::class, 'instance' ] );
+}
+// @codeCoverageIgnoreEnd
